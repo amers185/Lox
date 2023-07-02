702ed4534a3658b2c3a332e180dfaabbe532a65d
Syed Usama Amer <syedusamaamer@gmail.com>
2023-07-01 19:38:31 -0700

    Add functions

--- src/com/craftinginterpreters/lox/AstPrinter.java
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -19,6 +19,11 @@ class AstPrinter implements Expr.Visitor<String> {
     }
 
     @Override
+    public String visitCallExpr(Expr.Call expr) {
+        return null;
+    }
+
+    @Override
     public String visitVariableExpr(Expr.Variable expr) {
         return expr.name.lexeme;
     }
